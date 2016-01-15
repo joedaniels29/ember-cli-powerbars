fdcbb14607959012f51df2f7bb6e9adf07b8f066
Joseph Daniels <jdanie37@jh.edu>
2016-01-15 13:23:49 -0500

    fixes asset issue?

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
-var path = require('path');
-var checker = require('ember-cli-version-checker');
-var mergeTrees = require('broccoli-merge-trees');
-var pickFiles = require('broccoli-static-compiler');
+//var path = require('path');
+//var checker = require('ember-cli-version-checker');
+//var mergeTrees = require('broccoli-merge-trees');
+//var pickFiles = require('broccoli-static-compiler');
 
 module.exports = {
   name : 'ember-cli-powerbars',
